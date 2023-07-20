3f768fece56e0d00f4020535e4f82d05dd7af409
Usama Iqbal <uiqbal@cedrus.com>
2023-07-20 13:10:08 +0500

    feat: This program calculate the power of base and give result of the given numbers

---
+++ BaseExponent/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+public class exponent
+{
+    public static void Main()
+    {
+        int num;
+        int exp;
+                
+        Console.Write("Input Base number: ");
+        num = Convert.ToInt32(Console.ReadLine());
+        Console.Write("Input the Exponent : ");
+        exp = Convert.ToInt32(Console.ReadLine());
+        int value = 1;
+        
+        for (int i = 1; i <= exp; i++)
+            value = value * num;
+        Console.Write("The power of number is : " +value);
+       
+    }
+   
+}
